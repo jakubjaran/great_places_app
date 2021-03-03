647d8543a7e3b2bbddc6a8ade26a1517507e9c65
unknown <jakubjaran@gmail.com>
2021-03-03 10:55:02 +0100

    Set models, provider, places list screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './providers/places.dart';
+
+import './screens/places_list_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return ChangeNotifierProvider.value(
+      value: Places(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primaryColor: Colors.indigo,
+          accentColor: Colors.indigoAccent,
+        ),
+        home: PlacesListScreen(),
+      ),
     );
   }
 }
